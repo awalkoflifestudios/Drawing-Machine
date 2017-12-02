bf207f1d2c67d39f16a35f9af663433fecb5cff2
Alan Grover <awgrover@gmail.com>
2017-12-02 12:13:11 -0500

    add window2port.py

---
+++ window2port.py
@@ -0,0 +1,40 @@
+import sys,os
+import serial.tools.list_ports
+import serial
+
+# linux/windows
+if os.name == 'nt':
+    import msvcrt # windows
+elif os.name == 'posix':
+    import tty # linux
+else:
+    print "What os: %s" % os.name
+    exit(1)
+
+
+def getchar():
+    if os.name == 'nt':
+        return msvcrt.getch()
+    else:
+        return sys.stdin.read(1)
+
+
+print "Ports ",[port.name for port in serial.tools.list_ports.comports()]
+default_port = serial.tools.list_ports.comports()[0].name
+print "Which port? [%s] " % default_port,
+want_port = sys.stdin.readline().rstrip()
+if want_port == '':
+    want_port = default_port
+
+# 'list index out of range' means bad port name
+serial_port = serial.Serial( "/dev/" + want_port, 9600 ) # [port for port in serial.tools.list_ports.comports() if port.name == want_port][0] )
+
+if os.name == 'posix':
+    tty.setraw(sys.stdin.fileno())
+
+while (1):
+    c = getchar()
+    if c == '\x03':
+        exit(0)
+    serial_port.write(c)
+    print "'%c'\r" % c
