3ef14c3cd085ba657d617812826d25785370f663
awalkoflifestudios <33583302+awalkoflifestudios@users.noreply.github.com>
2017-12-02 13:09:34 -0500

    works from command line python

--- window2port.py
+++ window2port.py
@@ -3,8 +3,10 @@ import serial.tools.list_ports
 import serial
 
 # linux/windows
+print "os.name %s", os.name
 if os.name == 'nt':
-    import msvcrt # windows
+    import msvcrt  # windows
+    # import win32com.client
 elif os.name == 'posix':
     import tty # linux
 else:
@@ -18,16 +20,22 @@ def getchar():
     else:
         return sys.stdin.read(1)
 
+#if os.name =='nt':
+#    print "Ports ",[ usb.DeviceID for usb in wmi.InstancesOf("Win32_USBHub") ]
+#    exit(0)
+#elif os.name == 'posix':
+if True:
+    print "Ports ",[port.name for port in serial.tools.list_ports.comports()]
+    default_port = serial.tools.list_ports.comports()[0].name
+    print "Which port? [%s] " % default_port,
 
-print "Ports ",[port.name for port in serial.tools.list_ports.comports()]
-default_port = serial.tools.list_ports.comports()[0].name
-print "Which port? [%s] " % default_port,
 want_port = sys.stdin.readline().rstrip()
 if want_port == '':
     want_port = default_port
 
 # 'list index out of range' means bad port name
-serial_port = serial.Serial( "/dev/" + want_port, 9600 ) # [port for port in serial.tools.list_ports.comports() if port.name == want_port][0] )
+prefix = '/dev/' if os.name=='posix' else ''
+serial_port = serial.Serial( prefix + want_port, 9600 ) # [port for port in serial.tools.list_ports.comports() if port.name == want_port][0] )
 
 if os.name == 'posix':
     tty.setraw(sys.stdin.fileno())
@@ -36,5 +44,6 @@ while (1):
     c = getchar()
     if c == '\x03':
         exit(0)
-    serial_port.write(c)
-    print "'%c'\r" % c
+    elif ord(c) != 255: # windows returns 255 for "no char"
+        serial_port.write(c)
+        print "'%c'" % c,ord(c)
